8315dc802a5744dd0a139b25b76cf30010a35717
jhonk <jhonkoan110@gmail.com>
2021-01-14 12:31:45 +0300

    update

--- Тема2/calculator.js
+++ Тема2/calculator.js
@@ -1,5 +1,6 @@
 function Calculator(startValue) {
     try {
+        // Проверка, каким способом была вызвана функция, через 'new' или просто
         if (!new.target || (typeof startValue !== 'number' && isNaN(startValue))) {
             throw new UserException(
                 'Функция должна быть вызвана с оператором new, а аргументом ф-и должно быть число',
@@ -11,24 +12,36 @@ function Calculator(startValue) {
         return undefined;
     }
 
+    // Начальное значение
+    // Приоводится к строке, затем операции добавляют в эту строку
+    // символ операции и значение
     this.startValue = String(startValue);
 
+    // Сложение
     this.plus = function (value) {
         this.startValue = `${this.startValue} + ${value}`;
         return this;
     };
+
+    // Вычитание
     this.minus = function (value) {
         this.startValue = `${this.startValue} - ${value}`;
         return this;
     };
+
+    // Умножение
     this.multiply = function (value) {
         this.startValue = `${this.startValue} * ${value}`;
         return this;
     };
+
+    // Деление
     this.divide = function (value) {
         this.startValue = `${this.startValue} / ${value}`;
         return this;
     };
+
+    // Считает всю цепочку операций, приводя строку к вычисляемому выражению
     this.calculate = function () {
         this.startValue = eval(this.startValue);
         return this.startValue;
